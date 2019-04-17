f2320f25bb27d0b76c92343fe880ec6df61adfa7
Pil4283 <swswswchung@gmail.com>
2019-04-17 15:14:20 +0900

    Enum모음 클래스 추가

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,12 @@
+﻿
+public static class Enums
+{
+    /// <summary>
+    /// 알림창의 타입
+    /// </summary>
+    public enum NoticeType
+    {
+        Notice = 1,     //일방적 통보
+        Question = 2    //확인,취소
+    }
+}
